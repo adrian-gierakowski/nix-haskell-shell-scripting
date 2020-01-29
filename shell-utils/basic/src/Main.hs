4a82eeb0b5263d7293dc412b50fbf7d28c4f0b28
Adrian Gierakowski <agierakowski@gmail.com>
2020-01-29 01:15:21 +0000

    make nix-shell shebang self-contained

--- shell-utils/basic/src/Main.hs
+++ shell-utils/basic/src/Main.hs
@@ -1,5 +1,6 @@
 #! /usr/bin/env nix-shell
-#! nix-shell ../shell.nix -i runghc
+#! nix-shell -i runghc -p "ghc.withPackages (x: [ x.turtle ])"
+#! nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs/archive/9beb0d1ac2ebd6063efbdc4d3631f8ce137bbf90.tar.gz
 
 {-# LANGUAGE OverloadedStrings #-}
 
