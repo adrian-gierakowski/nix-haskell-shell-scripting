4ee834dd9b7ea17830af5589a847ef95a09be9bc
Adrian Gierakowski <agierakowski@gmail.com>
2020-01-29 01:20:15 +0000

    auto-reloading shebang with ghcid

--- shell-utils/basic/src/Main.hs
+++ shell-utils/basic/src/Main.hs
@@ -1,6 +1,8 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i runghc -p "ghc.withPackages (x: [ x.turtle ])"
+#! nix-shell -i "ghcid -c ghci -T main"
+#! nix-shell -p "ghc.withPackages (x: [ x.turtle ])"
 #! nix-shell -I nixpkgs=https://github.com/nixos/nixpkgs/archive/9beb0d1ac2ebd6063efbdc4d3631f8ce137bbf90.tar.gz
+
 
 {-# LANGUAGE OverloadedStrings #-}
 
